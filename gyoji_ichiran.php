4508adc403d44143cedccade20b768902c90d53b
NaokiYanagisawa <naokiyanagisawa00@gmail.com>
2012-12-03 16:14:17 +0900

    管理番号:18
    【課題】
    管理メニュー内　行事一覧及び休館日一覧ページにて　検索条件の開始日と終了日に全角文字を入力するとアラートが表示もしくは入力不可でないといけないのにエラー文が表示
    
    【修正内容】
    全角文字を入れたときアラート文を表示するようにしました。また、リセットボタンを押すとアラート文が出てしまう障害も修正
    【修正ファイル】
    gyoji_ichiran.php　ichiran_functions.php

--- gyoji_ichiran.php
+++ gyoji_ichiran.php
@@ -319,7 +319,7 @@ function search(){
 //
 
 echo <<< __searchbar__
-<form name="searchbar" method="POST" OnSubmit="return AllSerchCheck('IDsearch')">
+<form name="searchbar" method="POST" OnSubmit="return AllSerchCheck('IDsearch','kaishibi_search','syuryobi_search')">
 <div class="Kensaku-box">
 <table class ="table1-t1">
 	<tbody>
@@ -367,7 +367,7 @@ echo <<< __searchbar__
 <div class="Kensaku-btn">
 <input type="hidden" name="currentpage_start" value="1">
 <input type="submit" name="mode_serch" value="開始" class="button">
-<input type="submit" name="mode_serch" value="リセット" class="button">
+<input type="submit" name="mode_serch" value="リセット" class="button" onClick="document.searchbar.reset()">
 </div>
 </form>
 __searchbar__;
